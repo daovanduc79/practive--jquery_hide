3f6526a9fcca4703acdb300f603508cfdb0ce2d7
daovanduc <duc07092000@gmail.com>
2020-06-03 14:16:18 +0700

    up

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="urf-8">
+    <link href="css.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script>
         $(document).ready(function(){
@@ -12,7 +13,7 @@
                 $("p").show();
             });
             $("button").click(function(){
-                var div = $("div");
+                let div = $("div");
                 div.animate({left: '200px'}, "slow");
                 div.animate({fontSize: '100px'}, "slow");
             });
@@ -27,5 +28,25 @@
 <button>Start Animation</button>
 <p>By default, all HTML elements have a static position, and cannot be moved. To manipulate the position, remember to first set the CSS position property of the element to relative, fixed, or absolute!</p>
 <div style="background:#98bf21;height:100px;width:300px;position:absolute;">HELLO</div>
+
+<br><br><br><br><br><br><br><br><br><br><br>
+
+<div class="">
+    <div id="myDIV" class="header">
+        <h2 style="margin:5px">My To Do List</h2>
+        <input type="text" id="myInput" placeholder="Title...">
+        <span class="addBtn" onclick="newElement()">Add</span>
+    </div>
+
+    <ul id="myUL">
+        <li>Hit the gym</li>
+        <li>Pay bills</li>
+        <li>Meet George</li>
+        <li>Buy eggs</li>
+        <li>Read a book</li>
+        <li>Organize office</li>
+    </ul>
+</div>
+<script src="js.js"></script>
 </body>
 </html>
---
+++ js.js
@@ -0,0 +1,37 @@
+let myNodelist = document.getElementsByTagName("LI");
+for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+let close = document.getElementsByClassName("close");
+closeElement();
+function closeElement(){
+    let i;
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            let div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
+function newElement() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    closeElement();
+}
